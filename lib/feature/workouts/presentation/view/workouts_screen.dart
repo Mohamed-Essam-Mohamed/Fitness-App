c3e6f9b847fe32d6bf3b2addf9fe91e9401c1aad
Mj1234ba <bakriaweja@gmail.com>
2025-06-27 16:36:53 +0300

    feat(workout) implement ui design only

--- lib/feature/workouts/presentation/view/workouts_screen.dart
+++ lib/feature/workouts/presentation/view/workouts_screen.dart
@@ -1,12 +1,88 @@
+import 'package:fitness_app/core/constants/app_assets.dart';
+import 'package:fitness_app/core/constants/app_colors.dart';
 import 'package:flutter/material.dart';
 
-class WorkoutsScreen extends StatelessWidget {
+class WorkoutsScreen extends StatefulWidget {
   const WorkoutsScreen({super.key});
 
   @override
+  State<WorkoutsScreen> createState() => _WorkoutsScreenState();
+}
+
+class _WorkoutsScreenState extends State<WorkoutsScreen> {
+  int ind = 0;
+  List<String> list = ['Chest', 'Arm', 'Shoulder', 'Back', 'Legs', 'gluts'];
+  @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.blue,
-    );
+        decoration: const BoxDecoration(
+            image: DecorationImage(image: AssetImage(ImageAsset.thirdBg))),
+        child: Scaffold(
+          body: SafeArea(
+              child: Column(
+            children: [
+              Text(
+                'Gym',
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge!
+                    .copyWith(fontWeight: FontWeight.w600),
+              ),
+              const SizedBox(height: 24),
+              SizedBox(
+                width: double.infinity,
+                height: 40,
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: list.length,
+                    itemBuilder: (context, index) {
+                      return InkWell(
+                        splashColor: Colors.transparent,
+                        onTap: () {
+                          ind = index;
+                          setState(() {});
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 16),
+                          child: Container(
+                            padding: const EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                color: index == ind
+                                    ? AppColors.orange
+                                    : Colors.transparent),
+                            child: Center(
+                              child: Text(
+                                list[index],
+                                style: Theme.of(context).textTheme.bodyMedium,
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    }),
+              ),
+              const SizedBox(height: 24),
+              Expanded(
+                  child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            childAspectRatio: 1,
+                            mainAxisSpacing: 18,
+                            crossAxisSpacing: 17),
+                    itemBuilder: (context, index) {
+                      return Container(
+                        decoration: BoxDecoration(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.circular(20)),
+                      );
+                    }),
+              ))
+            ],
+          )),
+        ));
   }
 }
